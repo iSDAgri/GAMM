d47711a45d706186b57aaea76029e9af19e18e6a
mgwalsh <markusgwalsh@gmail.com>
2016-12-04 16:29:03 +0300

    data downlod

---
+++ ET_GH_NG_TZ maize prices.R
@@ -0,0 +1,19 @@
+#' AfSIS country wholesale maize price time series with GAMM's
+#' example data assembled from FAO (2010-2016) http://www.fao.org/giews/pricetool
+#' M. Walsh, December 2016
+
+# Required packages
+# install.packages(c("downloader","itsadug")), dependencies=TRUE)
+suppressPackageStartupMessages({
+  require(downloader)
+  require(itsadug)
+})
+
+# Data setup --------------------------------------------------------------
+# Create a data folder in your current working directory
+dir.create("Price_data", showWarnings=F)
+setwd("./Price_data")
+
+# Download 
+download("https://www.dropbox.com/s/pv68x94c9jfyo6z/AC_maize_prices.csv?dl=0", "AC_maize_prices.csv", mode="wb")
+mprice <- read.table("AC_maize_prices.csv", header=T, sep=",")
