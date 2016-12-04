d0fc3c377cc34c31719ef5c18df77f3789af0449
mgwalsh <markusgwalsh@gmail.com>
2016-12-04 19:31:23 +0300

    data dowload

--- ET_GH_NG_TZ maize prices.R
+++ ET_GH_NG_TZ maize prices.R
@@ -3,10 +3,11 @@
 #' M. Walsh, December 2016
 
 # Required packages
-# install.packages(c("downloader","itsadug")), dependencies=TRUE)
+# install.packages(c("downloader","itsadug","dygraphs")), dependencies=TRUE)
 suppressPackageStartupMessages({
   require(downloader)
   require(itsadug)
+  require(dygraphs)
 })
 
 # Data setup --------------------------------------------------------------
@@ -18,6 +19,7 @@ setwd("./Price_data")
 download("https://www.dropbox.com/s/pv68x94c9jfyo6z/AC_maize_prices.csv?dl=0", "AC_maize_prices.csv", mode="wb")
 mprice <- read.table("AC_maize_prices.csv", header=T, sep=",")
 
-# Generate mid-month pseudo-date
+# Generate mid-month pseudo-date & time series object
 mprice$date <- as.Date(paste(mprice$year, mprice$month, "15", sep = "-"))
+mpxt <- xts(x = mprice$price, order.by = mprice$date)
 
