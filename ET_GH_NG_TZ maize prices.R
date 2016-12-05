543694c88cc3ad9f9ba8ea3cb195fac056e6326b
mgwalsh <markusgwalsh@gmail.com>
2016-12-05 15:24:07 +0300

    GAMM tests

--- ET_GH_NG_TZ maize prices.R
+++ ET_GH_NG_TZ maize prices.R
@@ -1,14 +1,12 @@
-#' AfSIS country wholesale maize price time series with GAMM's
+#' AfSIS country wholesale maize price time series with GAMMs
 #' example data assembled from FAO (2010-2016) http://www.fao.org/giews/pricetool
 #' M. Walsh, December 2016
 
 # Required packages
-# install.packages(c("downloader","itsadug","dygraphs","xts")), dependencies=TRUE)
+# install.packages(c("downloader","itsadug")), dependencies=TRUE)
 suppressPackageStartupMessages({
   require(downloader)
   require(itsadug)
-  require(xts)
-  require(dygraphs)
 })
 
 # Data setup --------------------------------------------------------------
@@ -21,12 +19,17 @@ download("https://www.dropbox.com/?s/pv68x94c9jfyo6z/AC_maize_prices.csv?dl=0", 
 mprice <- read.table("AC_maize_prices.csv", header=T, sep=",")
 
 # GAMMs -------------------------------------------------------------------
-# country-level series
-m1 <- gam(price~ CC + s(rmonth, CC, bs="fs", m=1) + s(month, CC, bs="fs", m=1, k=12), data=mprice)
+# country-level model
+m1 <- gam(price~ CC + s(rmonth, CC, bs="fs", m=1) + s(month, CC, bs="fs", m=1), data=mprice)
 summary(m1)
 
-# market-level series
-m2 <- gam(price~ market + s(rmonth, market, bs="fs", m=1) + s(month, market, bs="fs", m=1, k=12), data=mprice)
+# market-level model
+m2 <- gam(price~ market + s(rmonth, market, bs="fs", m=1) + s(month, market, bs="fs", m=1), data=mprice)
 summary(m2)
 
-
+# country-level model adjusted for temporal correlation
+mprice <- start_event(mprice, column="rmonth", event="market")
+(valRho <- acf(resid(m1), plot=FALSE)$acf[2])
+m3 <- gam(price~ CC + s(rmonth, CC, bs="fs", m=1) + s(month, CC, bs="fs", m=1), data=mprice,
+          AR.start = mprice$start.event, rho=valRho)
+summary(m3)
